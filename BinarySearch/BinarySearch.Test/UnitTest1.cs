7f1f16db47889ffaa5e07c034e21beb2527fea50
Meggan <meggtriplett@gmail.com>
2019-03-21 01:42:13 -0700

    Created Binary Search method using wikipedia doc for psuedo code assistance

---
+++ BinarySearch/BinarySearch.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace BinarySearch.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+
+
+    }
+}
--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -2,11 +2,42 @@
 
 namespace BinarySearch
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+
+        //structured off of wikipedia psuedocode for Binary Search Algorithm
+        /// <summary>
+        /// Iterates through sorted array to find values matching the target search key. Outputs index of array element that matches so -1 when matching element does not exist. 
+        /// </summary>
+        /// <param name="sortedArray">An array of integers sorted from lowest element value to highest element value</param>
+        /// <param name="searchKey">An integer whose value you are looking to find if a matching array element's value exists</param>
+        /// <returns>Either the index of a matching element value or -1 if there are no equal values</returns>
+        public static int BinarySearch(int[] sortedArray, int searchKey)
+        {
+            int min = 0;
+            int last = sortedArray.Length - 1;
+
+            while (min <= last)
+            {
+                int middle = (min + last) / 2;
+
+                if(sortedArray[middle] < searchKey)
+                {
+                    min = middle + 1;
+                } else if(sortedArray[middle] > searchKey)
+                    {
+                        last = middle - 1;
+                    } else
+                        {
+                            return middle;
+                        } 
+            }
+            return -1;
+        }
+
     }
 }
